4215c68028d29301b94169dc2a0acc0522be0a2b
queenly9 <queenly9@naver.com>
2024-01-13 04:04:59 +0900

    날짜 및 시간 추가

--- App.tsx
+++ App.tsx
@@ -6,7 +6,39 @@ import {
   View,
 } from 'react-native';
 
+import {useState, useEffect} from 'react';
+
 function App(): JSX.Element {
+  const [, setCurrentDateTime] = useState(new Date());
+
+  const updateDateTime = () => {
+    setCurrentDateTime(new Date());
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatTime = () => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const year = currentTime.getFullYear();
+    const month = currentTime.getMonth() + 1;
+    const day = currentTime.getDate();
+
+    const formattedTime = `${year}년 ${month}월 ${day}일 ${
+      hours < 10 ? '0' : ''
+    }${hours}시 ${minutes < 10 ? '0' : ''}${minutes}분 ${
+      seconds < 10 ? '0' : ''
+    }${seconds}초`;
+
+    return formattedTime;
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
@@ -16,26 +48,22 @@ function App(): JSX.Element {
             alignSelf: 'flex-start',
             marginLeft: 15,
           }}>
-          <View style={styles.btn}>
-            <Text style={styles.txt}>Work</Text>
-          </View>
-          <View style={styles.btn}>
-            <Text style={styles.txt}>Study</Text>
-          </View>
-          <View style={styles.btn}>
-            <Text style={styles.txt}>Buy</Text>
-          </View>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btxt}>Work</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btxt}>Study</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btxt}>Buy</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.box}>
-          <View style={styles.todoWrapper}>
-            <TouchableOpacity style={styles.checkBtn}></TouchableOpacity>
-            <View style={styles.todo}></View>
+          <View style={styles.timeWrapper}>
+            <Text style={styles.ttxt}>오늘은 {formatTime()} 입니다</Text>
           </View>
-          <View style={styles.todoWrapper}>
-            <TouchableOpacity style={styles.checkBtn}></TouchableOpacity>
-            <View style={styles.todo}></View>
-          </View>
+
           <View style={styles.todoWrapper}>
             <TouchableOpacity style={styles.checkBtn}></TouchableOpacity>
             <View style={styles.todo}></View>
@@ -72,12 +100,11 @@ const styles = StyleSheet.create({
     shadowRadius: 15,
     shadowOpacity: 0.5,
   },
-  txt: {
+  btxt: {
     color: 'grey',
     fontSize: 18,
   },
   todoWrapper: {
-    //backgroundColor: 'grey',
     marginHorizontal: 20,
     height: 80,
     marginTop: 10,
@@ -96,6 +123,31 @@ const styles = StyleSheet.create({
     width: 270,
     height: 60,
     borderRadius: 20,
+  },
+  timeWrapper: {
+    //backgroundColor: 'red',
+    height: 30,
+    alignItems: 'center',
+  },
+  ttxt: {
+    fontSize: 15,
+    color: 'grey',
+  },
+  statusWrapper: {
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  bar: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginVertical: 10,
+    height: 10,
+  },
+  stxt: {
+    fontSize: 15,
+    color: 'grey',
   },
 });
 export default App;
