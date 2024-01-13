8248015320b535e7bfeeb2d9b75629d23b690fd8
queenly9 <queenly9@naver.com>
2024-01-13 09:39:26 +0900

    statusBar 추가 checkBtn 변경

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import {
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -7,7 +8,10 @@ import {
 } from 'react-native';
 
 import {useState, useEffect} from 'react';
+import {ProgressView} from '@react-native-community/progress-view';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+// @ts-ignore
 function App(): JSX.Element {
   const [, setCurrentDateTime] = useState(new Date());
 
@@ -58,16 +62,31 @@ function App(): JSX.Element {
             <Text style={styles.btxt}>Buy</Text>
           </TouchableOpacity>
         </View>
-
         <View style={styles.box}>
-          <View style={styles.timeWrapper}>
-            <Text style={styles.ttxt}>오늘은 {formatTime()} 입니다</Text>
-          </View>
+          <ScrollView>
+            <View style={styles.statusWrapper}>
+              <View style={styles.percentWrapper}>
+                <Text style={styles.stxt}>0%</Text>
+              </View>
+              <ProgressView
+                style={styles.statusBar}
+                progressTintColor="black"
+                trackTintColor="grey"
+                progress={0}
+              />
+            </View>
 
-          <View style={styles.todoWrapper}>
-            <TouchableOpacity style={styles.checkBtn}></TouchableOpacity>
-            <View style={styles.todo}></View>
-          </View>
+            <View style={styles.timeWrapper}>
+              <Text style={styles.ttxt}>오늘은 {formatTime()} 입니다</Text>
+            </View>
+
+            <View style={styles.todoWrapper}>
+              <TouchableOpacity>
+                <Icon name="heart-o" size={25} color="pink" />
+              </TouchableOpacity>
+              <View style={styles.todo}></View>
+            </View>
+          </ScrollView>
         </View>
       </View>
     </SafeAreaView>
@@ -112,12 +131,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  checkBtn: {
-    backgroundColor: 'lightblue',
-    borderRadius: 50,
-    height: 30,
-    width: 30,
-  },
   todo: {
     backgroundColor: 'lightgrey',
     width: 270,
@@ -139,15 +152,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
   },
-  bar: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    marginVertical: 10,
-    height: 10,
-  },
   stxt: {
     fontSize: 15,
     color: 'grey',
+  },
+  statusBar: {
+    width: 250,
+  },
+  percentWrapper: {
+    height: 60,
+    width: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //backgroundColor: 'red',
   },
 });
 export default App;
